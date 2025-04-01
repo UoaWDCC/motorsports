d0accb6d530070a7543f0b02924437ae9265a25f
rebeccafitzpatr <rebecca.fitzpatr@gmail.com>
2025-04-01 17:29:36 +1300

    release: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  
+  output: 'standalone',
+
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
