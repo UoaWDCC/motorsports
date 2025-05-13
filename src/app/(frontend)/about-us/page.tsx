92c723d1c6ad3fb6b0b45fe2083a91fa404f4da1
rebeccafitzpatr <rebecca.fitzpatr@gmail.com>
2025-05-13 22:23:52 +1200

    add about us row contents

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -11,10 +11,49 @@ export default function AboutUsPage() {
       </div>
 
       {/*customised content for the page*/}
-      <div className="about-us-row-1"></div>
-      <div className="about-us-row-2"></div>
+      <div className="content">
+        <div className="about-us-row-1">
+          <div className="about-us-row-1-image">
+            <Image
+              src="/images/20250412_094454.jpg"
+              className="about-us-image"
+              alt="About Us"
+              fill
+            />
+          </div>
+          <div className="about-us-row-1-text">
+            <h2>Lorem Ipsum Dolor sit amet</h2>
+            <p>Lorem ipsum dolor.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse.
+            </p>
+          </div>
+        </div>
+        <div className="about-us-row-1">
+          <div className="about-us-row-1-text">
+            <h2>Lorem Ipsum Dolor sit amet</h2>
+            <p>Lorem ipsum dolor.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate v.
+            </p>
+          </div>
 
-
+          <div className="about-us-row-1-image">
+            <Image
+              src="/images/20250412_094454.jpg"
+              className="about-us-image"
+              alt="About Us"
+              fill
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
