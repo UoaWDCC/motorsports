db275d6b9272641c8a8d0460e702ddc1b201fe40
rebeccafitzpatr <rebecca.fitzpatr@gmail.com>
2025-05-13 19:34:40 +1200

    add comments

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -3,11 +3,18 @@ import Image from 'next/image'
 export default function AboutUsPage() {
   return (
     <div className="content-page">
+      {/*reusable across similar page styles*/}
       <div className="background-image">
         <Image src="/images/20250412_094454.jpg" className="background-image" alt="About Us" fill />
-        <div className="background-gradient"></div>
+        <div className="background-gradient" />
         <h1>ABOUT US</h1>
       </div>
+
+      {/*customised content for the page*/}
+      <div className="about-us-row-1"></div>
+      <div className="about-us-row-2"></div>
+
+
     </div>
   )
 }
