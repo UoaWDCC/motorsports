c5527d883d1ba6e64986d597e81640a68dce0fca
Will Massam <wmas516@aucklanduni.ac.nz>
2025-05-19 23:59:13 +1200

    Added main body for event details
    - Added separator bar
    - Added description title and body
    - Added direct link to form as backup (currently links to google)

--- src/app/(frontend)/events-details/page.tsx
+++ src/app/(frontend)/events-details/page.tsx
@@ -27,10 +27,28 @@ export default function EventDetailsPage() {
         <div className="event-details-tile">
           <div className="event-details-content">
             <div className="event-details-headers">
-              <h2 style={{ display: 'inline-block', marginRight: '4rem' }}>TUE 15 FEB @ 12:30AM</h2>
-              <h2 style={{ display: 'inline-block' }}>2ND FLOOR KATE EDGAR</h2>
+              <h2 style={{ display: 'inline-block' }}>TUE 15 FEB @ 12:30AM</h2>
+              <h2 style={{ display: 'inline-block', marginLeft: '4rem' }}>2ND FLOOR KATE EDGAR</h2>
               <h1>VROOM 2025 NEWS</h1>
               <h3>Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur.</h3>
+              <div
+                style={{ display: 'inline-block', marginTop: '2rem', marginLeft: '2rem' }}
+                className="event-details-line"
+              ></div>
+              <h4 style={{ display: 'inline-block', marginTop: '3rem' }}>VROOM 2025 NEWS</h4>
+              <h3>
+                Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie
+                bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie bibendum
+                malesuada. Aenean vitae arcu consectetur.Morbi molestie bibendum malesuada. Aenean
+                vitae arcu consectetur.Morbi molestie bibendum malesuada. Aenean vitae arcu
+                consectetur.Morbi molestie bibendum malesuada.
+              </h3>
+              <a
+                style={{ display: 'inline-block', marginTop: '4rem', marginBottom: '3rem' }}
+                href="https://www.google.com"
+              >
+                Click for form.
+              </a>
             </div>
           </div>
         </div>
