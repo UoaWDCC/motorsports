23bb78e50c06a46e445024604511033627aefc2e
RiverMeteor6711 <cj15may@gmail.com>
2025-05-21 02:40:19 +1200

    started to style the navbar.

--- src/app/(frontend)/components/Navbar.tsx
+++ src/app/(frontend)/components/Navbar.tsx
@@ -4,47 +4,56 @@ import './styles.css'
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <Link href="/">
-        <button>Home</button>
-      </Link>
-
-      <Link href="/events">
-        <button>Events</button>
-      </Link>
-
-      {
-        <Link href="/volunteers">
-          <button>Volunteer</button>
+    <div className="navbarContainer">
+      <div className="navbar">
+        <img src="/images/logo.png" alt="Vroom Logo" className="logo" />
+<div className="nav-button-container">
+        <Link href="/" className="nav-button nav-home">
+          Home
         </Link>
-      }
 
-      {/* ExecsPage   */}
-      <Link href="/execs">
-        <button>Executives</button>
-      </Link>
+        <Link href="/events" className="nav-button">
+          Events
+        </Link>
 
-      {/* Gallery Page   */}
-      <Link href="/gallery">
-        <button>Gallery</button>
-      </Link>
+        {
+          <Link href="/volunteers" className="nav-button">
+            Volunteer
+          </Link>
+        }
 
-      <Link href="/about-us">
-        <button>About Us</button>
-      </Link>
+        {/* ExecsPage   */}
+        <Link href="/execs" className="nav-button">
+          Executives
+        </Link>
 
-      {/* Signup   */}
-      <Link href="/signup">
-        <button>Sign Up</button>
-      </Link>
+        {/* Gallery Page   */}
+        <Link href="/gallery" className="nav-button">
+          Gallery
+        </Link>
 
-      {/* Social media   */}
+        <Link href="/about-us" className="nav-button">
+          About Us
+        </Link>
 
-      {/*TODO: Add link to social media page*/}
-      {/*<Link href="/">*/}
-      <Link href="https://www.instagram.com/vroom_uoa/">
-        <button>Social Media</button>
-      </Link>
+        {/* Signup   */}
+        <Link href="/signup" className="nav-button">
+          Sign Up
+        </Link>
+
+        {/* Login   */}
+
+        {/*TODO: Add link to social media page*/}
+        {/*<Link href="/">*/}
+        <Link href="https://www.instagram.com/vroom_uoa/" className="nav-button">
+          Social Media
+        </Link>
+</div>
+        <Link href="/login" className="nav-login">
+          LOGIN
+        </Link>
+        
+      </div>
     </div>
   )
 }
