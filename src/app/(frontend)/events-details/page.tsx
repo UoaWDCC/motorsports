13eac930e453c2c62fa8a8c7b000a1042509a976
Will Massam <wmas516@aucklanduni.ac.nz>
2025-05-20 16:50:55 +1200

    Improved Code Style for Conventions
    - Removed unnecessary css
    - Reworked CSS to utilise margin for resizing
    - Replace individual calls to attributes of margin and replaced with 1 call
    - Removed border around google form

--- src/app/(frontend)/events-details/page.tsx
+++ src/app/(frontend)/events-details/page.tsx
@@ -19,39 +19,46 @@ export default function EventDetailsPage() {
       </div>
       {/*customised content for the page*/}
       <div className="content">
-        <div className="event-details-back">
-          <Link href="/events" className="event-details-back">
-            BACK
-          </Link>
-        </div>
-        <div className="event-details-tile">
-          <div className="event-details-content">
-            <div>
-              <h3>TUE 15 FEB @ 12:30AM</h3>
-              <h3 style={{ marginLeft: '4rem' }}>2ND FLOOR KATE EDGAR</h3>
-              <h2>VROOM 2025 NEWS</h2>
-              <p>Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur.</p>
-              <div className="event-details-line"></div>
-              <h4>VROOM 2025 NEWS</h4>
-              <p>
-                Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie
-                bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie bibendum
-                malesuada. Aenean vitae arcu consectetur.Morbi molestie bibendum malesuada. Aenean
-                vitae arcu consectetur.Morbi molestie bibendum malesuada. Aenean vitae arcu
-                consectetur.Morbi molestie bibendum malesuada.
-              </p>
-              <a
-                className="event-details-form-line"
-                href="https://docs.google.com/forms/d/e/1FAIpQLSeNOzbSG3_NEg7f2MI6gHIZT--DYf1WDyWIqf--vKpQTaUi5w/viewform?usp=dialog"
-              >
-                Click for form.
-              </a>
-              <iframe
-                className="event-details-form"
-                src="https://docs.google.com/forms/d/e/1FAIpQLSeNOzbSG3_NEg7f2MI6gHIZT--DYf1WDyWIqf--vKpQTaUi5w/viewform?embedded=true"
-              >
-                Loading…
-              </iframe>
+        <div className="event-details-content">
+          <div className="event-details-back">
+            <Link href="/events" className="event-details-back">
+              BACK
+            </Link>
+          </div>
+
+          <div className="event-details-tile">
+            <div className="event-details-tile-content">
+              <div>
+                <h2>TUE 15 FEB @ 12:30AM</h2>
+                <h2>2ND FLOOR KATE EDGAR</h2>
+                <h3>
+                  VROOM 2025 NEWS
+                  <p>Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur.</p>
+                </h3>
+                <div className="event-details-line"></div>
+                <h4>
+                  VROOM 2025 NEWS
+                  <p>
+                    Morbi molestie bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie
+                    bibendum malesuada. Aenean vitae arcu consectetur. Morbi molestie bibendum
+                    malesuada. Aenean vitae arcu consectetur. Morbi molestie bibendum malesuada.
+                    Aenean vitae arcu consectetur. Morbi molestie bibendum malesuada. Aenean vitae
+                    arcu consectetur. Morbi molestie bibendum malesuada.
+                  </p>
+                </h4>
+                <a
+                  className="event-details-form-link"
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSeNOzbSG3_NEg7f2MI6gHIZT--DYf1WDyWIqf--vKpQTaUi5w/viewform?usp=dialog"
+                >
+                  Click for form.
+                </a>
+                <iframe
+                  className="event-details-form"
+                  src="https://docs.google.com/forms/d/e/1FAIpQLSeNOzbSG3_NEg7f2MI6gHIZT--DYf1WDyWIqf--vKpQTaUi5w/viewform?embedded=true"
+                >
+                  Loading…
+                </iframe>
+              </div>
             </div>
           </div>
         </div>
