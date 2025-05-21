9927e45e8873afd72c37bb617597b25806857369
RiverMeteor6711 <cj15may@gmail.com>
2025-05-22 00:58:22 +1200

    finished navbar layout and styling.

--- src/app/(frontend)/components/Navbar.tsx
+++ src/app/(frontend)/components/Navbar.tsx
@@ -1,58 +1,66 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import './styles.css'
 
 export default function Navbar() {
+  const showExtraButtons = false // This hides the extra buttons we added initially.
   return (
     <div className="navbarContainer">
+        
       <div className="navbar">
-        <img src="/images/logo.png" alt="Vroom Logo" className="logo" />
-<div className="nav-button-container">
-        <Link href="/" className="nav-button nav-home">
-          Home
+        <Link href="/">
+        <Image src="/images/logo.png" alt="Vroom Logo" className="logo" width={80} height={75.47} />
         </Link>
-
-        <Link href="/events" className="nav-button">
-          Events
-        </Link>
-
-        {
-          <Link href="/volunteers" className="nav-button">
-            Volunteer
+        <div className="nav-button-container">
+          <Link href="/" className="nav-button nav-home">
+            Home
           </Link>
-        }
 
-        {/* ExecsPage   */}
-        <Link href="/execs" className="nav-button">
-          Executives
-        </Link>
+          <Link href="/events" className="nav-button">
+            Events
+          </Link>
 
-        {/* Gallery Page   */}
-        <Link href="/gallery" className="nav-button">
-          Gallery
-        </Link>
+          {/* Gallery Page   */}
+          <Link href="/gallery" className="nav-button">
+            Gallery
+          </Link>
 
-        <Link href="/about-us" className="nav-button">
-          About Us
-        </Link>
+          <Link href="/about-us" className="nav-button">
+            About Us
+          </Link>
 
-        {/* Signup   */}
-        <Link href="/signup" className="nav-button">
-          Sign Up
-        </Link>
+          {showExtraButtons && (
+            <>
+              <Link href="/volunteers" className="nav-button">
+                Volunteer
+              </Link>
 
-        {/* Login   */}
+              {/* ExecsPage   */}
+              <Link href="/execs" className="nav-button">
+                Executives
+              </Link>
+              
+              {/* Signup   */}
+              <Link href="/signup" className="nav-button">
+                Sign Up
+              </Link>
 
-        {/*TODO: Add link to social media page*/}
-        {/*<Link href="/">*/}
-        <Link href="https://www.instagram.com/vroom_uoa/" className="nav-button">
-          Social Media
-        </Link>
-</div>
+              {/* Login   */}
+
+              {/*TODO: Add link to social media page*/}
+              {/*<Link href="/">*/}
+              <Link href="https://www.instagram.com/vroom_uoa/" className="nav-button">
+                Social Media
+              </Link>
+            </>
+          )}
+        </div>
+        <div className="nav-login-container">
         <Link href="/login" className="nav-login">
           LOGIN
         </Link>
-        
+        </div>
       </div>
     </div>
   )
