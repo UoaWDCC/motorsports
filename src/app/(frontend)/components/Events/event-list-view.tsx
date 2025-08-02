f39c6afc2e599081ccae9316f4a029d86f2b9991
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2025-08-03 09:30:08 +1200

    fetch events fetch with previous and upcoming

--- src/app/(frontend)/components/Events/event-list-view.tsx
+++ src/app/(frontend)/components/Events/event-list-view.tsx
@@ -5,13 +5,20 @@ import styles from './events.module.css'
 import { EventListViewProps, TEvent } from '../../types/events'
 
 const EventListView = ({ setShowCalendar, showCalendar, events }: EventListViewProps & { events: TEvent[] }) => {  
+  
+  const now = new Date()
+
+  const previousEvents = events.filter(event => new Date(event.dateEnd) < now)
+
+  const upcomingEvents = events.filter(event => new Date(event.dateEnd) >= now)
+  
   return (
     <section className={styles.EventMain}>
       <Header text="events" />
 
       <div className={styles.EventBody}>
-        <Events events={events} setShowCalendar={ setShowCalendar } showCalendar={ showCalendar} type="upcoming" />
-        <Events events={events} setShowCalendar={ setShowCalendar } showCalendar={ showCalendar} type="previous" />
+        <Events events={upcomingEvents} setShowCalendar={ setShowCalendar } showCalendar={ showCalendar} type="upcoming" />
+        <Events events={previousEvents} setShowCalendar={ setShowCalendar } showCalendar={ showCalendar} type="previous" />
       </div>
     </section>
   )
