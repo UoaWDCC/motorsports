5417525fb090b9637dafc48e2b0b0cf91e612f2b
Will Massam <wmas516@aucklanduni.ac.nz>
2025-09-22 00:47:15 +1200

    Added basic leaderboard page

---
+++ src/app/(frontend)/leaderboard/page.tsx
@@ -0,0 +1,39 @@
+import './styles.css'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Leaderboard from '../components/Leaderboard/Leaderboard'
+export default function LeaderboardPage() {
+  return (
+    <div className="content-page">
+      {/*copied top header*/}
+      <div className="background-image">
+        <Image
+          src="/images/20250412_094454.jpg"
+          className="background-image"
+          alt="Event Details"
+          fill
+        />
+        <div className="background-gradient" />
+        <h1>Leaderboard</h1>
+      </div>
+      {/*customised content for the page*/}
+      <div className="content">
+        <div className="leaderboard-content">
+          <div className="button-back">
+            <Link href="/" className="button-back">
+              BACK
+            </Link>
+          </div>
+
+          <div className="leaderboard-tile">
+            <div className="leaderboard-tile-content">
+              <Leaderboard></Leaderboard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
